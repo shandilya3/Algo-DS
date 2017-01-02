1680c65710796dbaf5ba7abad4623f2b3d0844d2
Shandilya <happyshandilya@gmail.com>
2017-01-02 01:11:14 -0700

    Two Sum
    Given an array of integers, find two numbers such that they add up to a specific target number.

--- Program.cs
+++ Program.cs
@@ -1,44 +1,41 @@
-﻿using System;
+﻿//Given an array of integers, find two numbers such that they add up to a specific target number.
+using System;
 using System.Collections.Generic;
 namespace ConsoleApplication
 {
     public class Program
     {
+       public static  int  i = 0;
         public static void Main(string[] args)
         {
-            if(true==IsHappy(20))
-            {
-                Console.WriteLine("Happy Number ");
-            }
-            else
-            {
-                Console.WriteLine("This is not a Happy Number");
-            }
+            Program obj = new Program();
+           int[] numbers ={1,4,45,6,8,10};
+           int target =16;
+          int[] res= obj.twoSum(numbers ,  target);
+          foreach(int i in res)
+          {
+            Console.WriteLine(numbers[i]);
+          }
+        
         }
-        public  static Boolean IsHappy(int n )
-        {
-         HashSet<int> set = new HashSet<int>();
-            while(!set.Contains(n))
-            {
-                set.Add(n);
-                n = GetSum(n);
-                if(n==1)
-                   return true;
-            }           
-            return false;
-        }
-        public static int GetSum(int n)
-        {
-            int sum = 0;
-            while(n>0)
-            {
-                sum = sum+ (n%10) * (n%10);
-                Console.WriteLine("Sum"+sum);
-                n = n/10;
-                Console.WriteLine("n ="+n);
-            }
-            return sum;
-        }
+     
+        public int[] twoSum(int[] numbers, int target) {
+                Dictionary<int, int> dict = new Dictionary<int, int>();
+                int[] result = new int[2];
+            
+                for (int i = 0; i < numbers.Length; i++) {
+                    if (dict.ContainsKey(numbers[i])) {
+                    int index = dict[numbers[i]];
+                        result[0] = index ;
+                        result[1] = i;
+                        break;
+                    } else {
+                        dict.Add(target - numbers[i], i);
+                    }
+                }
+            
+            return result;
+        }  
+      
     }
 }
-//Reference/Source LeetCode and Prgram Creek/
